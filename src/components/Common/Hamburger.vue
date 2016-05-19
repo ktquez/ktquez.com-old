1abf029d692a46072d5b086a3e64fa8104565c54
ktquez <ktquez@gmail.com>
2016-05-18 23:43:13 -0300

    Nav mobile

---
+++ src/components/Common/Hamburger.vue
@@ -0,0 +1,84 @@
+<template>
+    <div class="hamburger" @click="active" :class="{'-active': isActive}" role="button">
+      <span class="inner"></span>
+    </div>    
+</template>
+
+<script>
+  // utilizar o vuex para avisar do click no hamburguer
+  export default {
+    data () {
+      return {
+        isActive: false
+      }
+    },
+    methods: {
+      active () {
+        this.isActive = !this.isActive
+      }
+    }
+  }
+</script>
+
+<style lang="postcss">
+  .hamburger {
+    width: 30px;
+    height: 20px;
+    margin-top: 26px;
+    margin-left: 25px;
+    cursor: pointer;
+    position: relative;
+    float: left;
+  
+    &.-active > .inner {
+      transform: rotate(45deg);
+      transition: all .5s .3s;      
+    }
+
+    &.-active > .inner:before {
+      top: 0;
+      transform: rotate(-90deg);
+      transition: all .5s .3s;
+    }
+
+    &.-active > .inner:after {
+      bottom: 0;
+      opacity: 0;
+      transition: all .3s;
+    }
+
+    & > .inner {
+      margin-top: 10px;
+    }
+
+    &:hover > .inner, 
+    &:hover > .inner:before,
+    &:hover > .inner:after{
+      background-color: #ff7070;
+    }
+    
+    & > .inner,
+    & > .inner:before,
+    & > .inner:after {
+      position: relative;
+      width: 30px;
+      height: 1px;
+      float:left;
+      background-color: #999;
+      transition: all .5s;
+    }
+
+    & > .inner:before,
+    & > .inner:after {
+      content: '';
+    }
+  
+    & > .inner:before {
+      top: -10px;
+    }
+
+    & > .inner:after {
+      bottom: -9px;
+    }
+  }
+</style>
--- src/components/Common/NavMobile.vue
+++ src/components/Common/NavMobile.vue
@@ -1,11 +1,43 @@
 <template>
-    <div></div>   
+    <div class="menu-mobile abs">
+      <div class="inner"></div>
+      <hamburger></hamburger>
+    </div>   
 </template>
 
 <script>
-  export default {}
+  // utilizar o vuex para saber quando o click no hamburger foi disparado
+  import Hamburger from './Hamburger'
+  export default {
+    components: { Hamburger }
+  }
 </script>
 
 <style lang="postcss">
-    
+    .menu-mobile {
+      left: -235px;
+      top: 0;
+      z-index: 6;
+      height: 100vh;
+      width: 290px;
+
+      &.-show {
+        left: 0;
+      }
+
+      & > .inner {
+        width: 235px;
+        float: left;
+        height: 100%;
+        position: relative;
+        background-color: #fafafa;
+        box-shadow: 20px -1px 20px -20px color(black alpha(10%));
+        visibility: hidden;
+      }
+
+      &.-show > .inner {
+        visibility: visible;
+      }
+    }
+
 </style>
