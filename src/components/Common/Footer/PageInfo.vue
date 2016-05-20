52748cea65b421b446b9d7746c7d25f34db4df36
ktquez <ktquez@gmail.com>
2016-05-20 12:05:33 -0300

    Add Footer

--- src/components/Common/Footer/PageInfo.vue
+++ src/components/Common/Footer/PageInfo.vue
@@ -1,11 +1,42 @@
 <template>
-    <!-- template html here -->    
+  <small class="info fullX tac">
+    &copy; {{ year }} - Build with 
+    <span class="heart">&hearts;</span>
+    <a class="link" href="https://vuejs.org/" title="Vue.js - Reactive Components for Modern Web Interfaces">
+      Vue.js
+    </a>, API RESTful by 
+    <a class="link" href="http://jexia.com/" title="Jexia - Build RESTful APIs without writing a single line of code">
+      Jexia
+    </a>
+    and deployed on
+    <a class="link" href="https://surge.sh/" title="Surge - Static web publishing for Front-End Developers">
+      Surge
+    </a>
+    </small>    
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        year: '2016'
+      }
+    }
+  }
 </script>
 
 <style lang="postcss">
-    /* style here */
+  .info {
+    font-size: 12px;
+    margin-top: 20px;
+    margin-bottom: 10px;
+
+    & > .heart {
+      color: red;
+    }
+
+    & > .link {
+      color: #4ac6b7;
+    }
+  }
 </style>
--- src/components/Common/Footer/index.vue
+++ src/components/Common/Footer/index.vue
@@ -1,11 +1,57 @@
 <template>
-    <!-- template html here -->    
+  <footer class="footer fullX">
+    <div class="box-center6 center">
+      <menu :class="['navbar-footer']"></menu>         
+      <page-info></page-info>               
+    </div>
+  </footer>
 </template>
 
 <script>
-  export default {}
+  import Menu from '../NavBar'
+  import PageInfo from './PageInfo'
+  export default {
+    components: {
+      Menu,
+      PageInfo
+    }
+  }
 </script>
 
 <style lang="postcss">
-    /* style here */
+
+  :root {
+    @custom-media --small-viewport (max-width: 480px);     
+  }
+
+  .footer {
+    margin-top: 20px;
+    padding-bottom: 20px;
+    padding-top: 10px;
+
+    & > .center {
+      border-top: 1px solid #e8e8e8;
+    }
+
+  }
+
+  .navbar-footer {
+    margin-top: 14px;
+  }
+
+   @media (--small-viewport) {
+    .navbar-footer .item {
+      float: left;
+      width: 100%;
+      text-align:center;
+      padding: 20px 0;
+      background: #efefef;
+      margin-bottom: 2px;
+
+      &:hover {
+        color: #4ac6b7;
+      }
+    }
+  }
+
 </style>
