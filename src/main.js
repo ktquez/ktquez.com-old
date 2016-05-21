bf072bff1a6278fc8a9a8bd0ab1451ee0bbb2a82
ktquez <ktquez@gmail.com>
2016-05-20 22:10:48 -0300

    Add Vue Resource

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 import App from './components/App'
 import { routes } from './routes'
 
 Vue.use(VueRouter)
+Vue.use(VueResource)
 
 const router = new VueRouter({
   history: true,
