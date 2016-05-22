d6d573bcbe730d887e8c2cfcd35c20d50e3a3eca
ktquez <ktquez@gmail.com>
2016-05-22 01:44:03 -0300

    Component TopPage

---
+++ src/components/Pages/TopPages.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="tac">
+    <div class="c-m12 cpl top-page">
+      <strong class="fullX title">{{ title }}</strong>
+      <div class="fullX description">
+        <p class="box-center7 lspacing">
+          {{ description }}
+        </p>              
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      description: {
+        type: String
+      }
+    }
+  }
+</script>
+
+<style lang="postcss">
+  .top-page {
+    margin-bottom: 20px;
+    margin-top: 20px;
+
+    & > .title {
+      font-size: 25px;
+      margin-bottom: 10px;
+    }
+
+    & > .description {
+      margin-top: 10px;
+      font-weight: 300;
+      font-size: 14px;
+      color: #666;
+    }
+  }
+</style>
