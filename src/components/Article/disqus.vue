a2fb6a5a2b5fe76135d36e752ff42c3a4d62305e
ktquez <ktquez@gmail.com>
2016-05-25 14:21:38 -0300

    Initial config component disqus

--- src/components/Article/disqus.vue
+++ src/components/Article/disqus.vue
@@ -1,1 +1,30 @@
-disqus
+<template>
+  <div id="disqus_thread"></div> 
+</template>
+
+<script>
+  export default {
+    props: {
+      shortname: {
+        type: String,
+        required: true
+      }
+    },
+    ready () {
+      const disqusShortname = this.shortname
+      setTimeout(() => {
+        (() => {
+          let dsq = document.createElement('script')
+          dsq.type = 'text/javascript'
+          dsq.async = true
+          dsq.setAttribute('id', 'embed-js-disqus')
+          dsq.src = 'http://' + disqusShortname + '.disqus.com/embed.js'
+          document.getElementsByTagName('body')[0].appendChild(dsq)
+        })()
+      }, 1000)
+    }
+  }
+</script>
+
+<style lang="postcss">
+</style>
--- src/components/Article/index.vue
+++ src/components/Article/index.vue
@@ -5,9 +5,19 @@
         <h1 class="title -post fullX tac top-page margin-section">Build components with Vue.js and Webpack</h1>
         <separate></separate>     
         <post class="post fullX" :post="post"></post>
-        <aside role="complementary" class="info-post">
-          
+        <aside role="complementary" class="fullX info-post">
+          <div class="c-m12 c-t8 cpl">
+            <div class="fl author">
+              <author></author>              
+            </div>
+          </div>
+          <div class="c-m12 c-t4 cpl">
+            <tags v-for="tag in tags" :item="tag"></tags>            
+          </div>
         </aside>
+        <section class="comments">
+          <disqus :shortname="shotname_disqus"></disqus>
+        </section>
       </article>
     </section> 
   </main>
@@ -16,15 +26,23 @@
 <script>
   import Separate from '../Common/Separate'
   import Post from './Post'
+  import Tags from '../Common/Tags'
+  import Author from './Author'
+  import Disqus from './Disqus'
   export default {
     data () {
       return {
-        post: ''
+        post: '',
+        shotname_disqus: 'ktquez',
+        tags: ['php', 'vue', 'laravel', 'html', 'css3']
       }
     },
     components: {
       Separate,
-      Post
+      Post,
+      Tags,
+      Author,
+      Disqus
     },
     route: {
       data (transition) {
@@ -41,8 +59,35 @@
   }
 </script>
 
-<style scoped>
-  .title.-post {
-    font-size: 45px;
+<style lang="postcss" scoped>
+  :root {
+    --border-color: #e8e8e8;
+    @custom-media --small-viewport (max-width: 480px);     
   }
+  .title.-post { font-size: 45px; }
+  
+  .info-post {
+    padding: 30px 0;
+    margin: 30px 0;
+    border-bottom: 1px solid var(--border-color);
+    border-top: 1px solid var(--border-color);
+  }
+
+  .author {
+    margin-right: 20px;
+    width: calc(100% - 20px);
+    border-right: 1px solid var(--border-color);    
+  }
+  
+  @media (--small-viewport) {
+    .author {
+      margin-bottom: 30px;
+      margin-right: 0;
+      padding-bottom: 30px;      
+      width: 100%;      
+      border-right: none;
+      border-bottom: 1px solid var(--border-color);      
+    }
+  }
+  
 </style>
