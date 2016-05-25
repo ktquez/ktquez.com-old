ee8e6022129890cec161271a8ee1f8c1d4c74198
ktquez <ktquez@gmail.com>
2016-05-25 11:20:19 -0300

    Update component tag

--- src/components/Common/Tags.vue
+++ src/components/Common/Tags.vue
@@ -1,10 +1,27 @@
 <template>
-  <div>TAGS</div>
+  <span class="tag">{{ item }}</span>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: {
+      item: {
+        type: String
+      }
+    }
+  }
 </script>
 
-<style lang="postcss">
+<style lang="postcss" scoped>
+  .tag {
+    margin-right: 10px;
+    margin-bottom: 10px;
+    padding: 6px 12px 5px;
+    float: left;
+    font-size: 12px;
+    text-transform: uppercase;
+    border: 1px solid #4AC6B7;
+    border-radius: 350px;
+    color: #4AC6B7;
+  }
 </style>
