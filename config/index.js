d530945bc7a161c92900511e5ece8c357ac702ac
ktquez <ktquez@gmail.com>
2016-06-28 12:28:59 -0300

    Update domain

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ var path = require('path')
 
 module.exports = {
   build: {
-    url: 'http://ktquez.surge.sh',
+    url: 'http://ktquez.com',
     env: require('./prod.env'),
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
